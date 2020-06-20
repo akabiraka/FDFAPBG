d32e70cbfa1d3c61721f8d3df6d52c48ffdaefbf
Anowarul Kabir <akabir0101@gmail.com>
2020-06-20 17:46:02 -0400

    pdb list is read and downloaded 1by1 using biopython

--- configs/general_config.py
+++ configs/general_config.py
@@ -1,5 +1,19 @@
+# data directories
 DATA_DIR = "data/"
+PDB_DIR = DATA_DIR + "pdbs/"
 
+# input files
+ALL_PDB_IDS = DATA_DIR + "all_pdb_ids_tiny.txt"
+BAD_PDB_IDS = DATA_DIR + "bad_pdb_ids.txt"
+GOOD_PDB_IDS = DATA_DIR + "good_pdb_ids.txt"
+
+# file format
+CIF = 'mmCif'
+
+# file extensions
+DOT_CIF = ".cif"
+
+# output directories
 OUTPUT_DIR = "outputs/"
 OUTPUT_IMAGES_DIR = OUTPUT_DIR + "images/"
 OUTPUT_MODELS_DIR = OUTPUT_DIR + "models/"
---
+++ datasets/data_generator.py
@@ -0,0 +1,39 @@
+import sys
+sys.path.append('../FDFAPBG')
+
+from Bio.PDB import *
+
+import configs.general_config as CONFIGS
+
+class DataGenerator(object):
+    def __init__(self):
+        super(DataGenerator, self).__init__()
+        print("alhumdulillah")
+
+        self.pdbl = PDBList()
+
+    def get_pdb_id(self, line):
+        """
+        Given a line where first item is a pdb_id with chain_id like '1A62A',
+        this method returns the pdb_id and chain_id. If ther are multiple chain_ids 
+        like '1A62ACD", it only returns first chain id like 'A'.
+        """
+        line = line.split()
+        pdb_id = line[0][:4].lower()
+        chain_id = line[0][4]
+        return pdb_id, chain_id
+
+    def download(self, pdb_code):
+        """
+        Download protein data in .cif format in CONFIGS.PDB_DIR.
+        """
+        self.pdbl.retrieve_pdb_file(pdb_code, pdir=CONFIGS.PDB_DIR, file_format=CONFIGS.CIF)
+
+generator = DataGenerator()
+file_content = open(CONFIGS.ALL_PDB_IDS, "r")
+for i, line in enumerate(file_content):
+    print("{}th protein:".format(i+1))
+    pdb_id, chain_id = generator.get_pdb_id(line)
+    pdb_with_chain = pdb_id + chain_id
+    print(pdb_id, chain_id)
+    generator.download(pdb_id)
