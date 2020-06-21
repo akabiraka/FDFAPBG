8549ef107725cae276349c43884d149d3e3f0813
Anowarul Kabir <akabir0101@gmail.com>
2020-06-20 22:59:00 -0400

    scaling withing a boundary function added

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -12,3 +12,8 @@ def save_contact_map(np_array, pdb_code):
     file = CONTACT_MAP_DIR + pdb_code
     save_tensor(torch.tensor(np_array), file + DOT_PT)
 
+def scale(X, x_min, x_max):
+    nom = (X-X.min(axis=0))*(x_max-x_min)
+    denom = X.max(axis=0) - X.min(axis=0)
+    denom[denom==0] = 1
+    return x_min + nom/denom 
