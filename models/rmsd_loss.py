ceec8aa57a4e9154906407e3245a5f197d0cf3c2
Anowarul Kabir <akabir0101@gmail.com>
2020-06-27 02:37:11 -0400

    rmsd loss is computed using quaternion and torchProteinLibrary

--- models/rmsd_loss.py
+++ models/rmsd_loss.py
@@ -1,42 +1,73 @@
-import sys
-sys.path.append('../FDFAPBG')
-
 import torch
 import torch.nn as nn
-from TorchProteinLibrary import RMSD
 
-class RMSDLoss(nn.Module):
+class RMSD_loss(nn.Module):
     """
-    Rigid body alignment loss using RMSD (root mean squared deviation) loss.
+    Code idea is taken from:
+    [1] https://towardsdatascience.com/tensorflow-rmsd-using-tensorflow-for-things-it-was-not-designed-to-do-ada4c9aa0ea2
+    [2] https://github.com/mdtraj/tftraj/blob/master/tftraj/rmsd.py
     """
+    def __init__(self):
+        super(RMSD_loss, self).__init__()
 
-    def __init__(self, device):
-        super(RMSDLoss, self).__init__()
-        self.device = device
-        if 'cuda' in device:
-            self.rmsd = RMSD.Coords2RMSD().cuda()
-        else:
-            self.rmsd = RMSD.Coords2RMSD()
+    def optimal_rotational_quaternion(self, r):
+        """Just need the largest eigenvalue of this to minimize RMSD over rotations
+        
+        References
+        ----------
+        [1] http://dx.doi.org/10.1002/jcc.20110
+        """
+        # @formatter:off
+        return [
+            [r[0][0] + r[1][1] + r[2][2], r[1][2] - r[2][1], r[2][0] - r[0][2], r[0][1] - r[1][0]],
+            [r[1][2] - r[2][1], r[0][0] - r[1][1] - r[2][2], r[0][1] + r[1][0], r[0][2] + r[2][0]],
+            [r[2][0] - r[0][2], r[0][1] + r[1][0], -r[0][0] + r[1][1] - r[2][2], r[1][2] + r[2][1]],
+            [r[0][1] - r[1][0], r[0][2] + r[2][0], r[1][2] + r[2][1], -r[0][0] - r[1][1] + r[2][2]],
+        ]
 
+    def translate(self, x):
+        """
+        x: (batch_size, n_atoms, 3)
+        """
+        x_translated = x - x.mean(dim=1, keepdim=True)
+        return x_translated
+
+    def squared_deviation(self, y_prime, y):
+        """
+        y_prime, y: (n_atoms, 3)
+        """
+        R = torch.matmul(y_prime.t(), y) # ordinary cross-correlation of xyz coordinates. (3, 3)
+        R_parts = [ torch.unbind(t) for t in torch.unbind(R)]
+        F_parts = self.optimal_rotational_quaternion(R_parts)
+        F = torch.tensor(F_parts)
+        # backward pass in only supported in symeig
+        # returned eigenvalues are in acending order
+        vals, vecs = torch.symeig(F, eigenvectors=True)
+        lamx = vals[-1] # getting the max eigen value
+        sd = torch.sum(y_prime**2 + y**2) - 2 * lamx
+        return sd
+
+    def rmsd(self, y_prime, y):
+        """
+        y_prime, y: (batch_size, n_atoms, 3)
+        """
+        batch_size, n_atoms, d = y_prime.shape
+        y_prime_translated = self.translate(y_prime)
+        y_translated = self.translate(y)
+        total_sd = 0.0
+        for i in range(batch_size):
+            total_sd += self.squared_deviation(y_prime[i], y[i]) 
+        return torch.sqrt(total_sd/n_atoms)
 
     def forward(self, y_prime, y):
         """
-        y_prime: (batch_size, n_coords, d) d-dimensional coordinates
-        y: (batch_size, n_coords, d) d-dimensional coordinates
+        y_prime: (batch_size, n_coords, 3) 
+        y: (batch_size, n_coords, 3)
         """
-        batch_size, n_coords, d = y_prime.shape
-        n_atoms = torch.ones(batch_size, dtype=torch.int) * n_coords
-        y_prime = y_prime.view(batch_size, n_coords * d)
-        y = y.view(batch_size, n_coords * d)
+        return self.rmsd(y_prime, y)
 
-        return self.rmsd(y_prime, y, n_atoms).mean()
-
-# # working example
-# y = torch.randn(5, 5, 3) # ground truth value
-# y_prime = torch.randn(5, 5, 3, requires_grad=True) # predicted value
-# y_prime = y_prime + torch.randn(5, 5, 3)
-
-# criterion = RMSDLoss(device='cpu')
-# loss = criterion(y_prime, y)
+# y = torch.randn((30, 256, 3))
+# y_prime = torch.randn((30, 256, 3), requires_grad=True)
+# rmsd_loss = RMSD_loss()
+# loss = rmsd_loss(y_prime, y)
 # print(loss)
-# loss.backward()
