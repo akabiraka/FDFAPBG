9478b6487c5fb5a034a2f6c2e14de3b38d106fcf
Anowarul Kabir <akabir0101@gmail.com>
2020-07-19 22:33:05 -0400

    3 rmsd computation equality proved

--- utils/our_rmsd_vs_bio_rmsd_prover.py
+++ utils/our_rmsd_vs_bio_rmsd_prover.py
@@ -4,8 +4,13 @@ sys.path.append("../FDFAPBG")
 import numpy as np
 import torch
 from models.rmsd_loss import RMSD_loss
+from models.rmsd_loss_1 import RMSDLoss
 from Bio.SVDSuperimposer import SVDSuperimposer
 from Bio.PDB.Superimposer import Superimposer
+
+sup = SVDSuperimposer()
+myRmsdLoss = RMSD_loss()
+proteinTorchLibRMSDLoss = RMSDLoss(device='cpu')
 
 for row in [16, 32, 64, 128, 256]:
     size = (row, 3)
@@ -13,17 +18,17 @@ for row in [16, 32, 64, 128, 256]:
     for i in range(10):
         x = np.random.uniform(low=-30.0, high=30.0, size=size)
         y = np.random.uniform(low=-30.0, high=30.0, size=size)
-
-        sup = SVDSuperimposer()
-        sup.set(x/100, x)
+        
+        # x = np.random.randn(size[0], size[1])
+        # y = np.random.randn(size[0], size[1])
+        
+        sup.set(x, y)
         sup.run()
         bio_rmsd = sup.get_rms()
         
-        # Superimposer uses SVDSuperimposer
-        # sup = Superimposer() 
-        # sup.set_atoms(x , x)
-        # bio_rmsd = sup.rms 
-
-        rmsdLoss = RMSD_loss()
-        my_rmsd = rmsdLoss.rmsd(torch.tensor(x).unsqueeze(dim=0), torch.tensor(y).unsqueeze(dim=0))
-        print("bio-rmsd: {:.3f}, our-rmsd: {:.3f}, diff: {:.3f}".format(bio_rmsd, my_rmsd.item(), bio_rmsd-my_rmsd.item()))
+        my_rmsd = myRmsdLoss(torch.tensor(x).unsqueeze(dim=0), torch.tensor(y, requires_grad=True).unsqueeze(dim=0))
+        
+        ptb_rmsd = proteinTorchLibRMSDLoss(torch.tensor(x).unsqueeze(dim=0), torch.tensor(y, requires_grad=True).unsqueeze(dim=0))
+        
+        # print(bio_rmsd, my_rmsd, ptb_rmsd)
+        print("bio-rmsd: {:.3f}, our-rmsd: {:.3f}, ptb_rmsd: {:.3f}".format(bio_rmsd, my_rmsd, ptb_rmsd))
